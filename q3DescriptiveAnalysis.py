3e83b582c3ccb3cef13b46a3c48ca6afa8820537
Sohail Uddin Syed <syedsohail601@gmail.com>
2023-11-21 22:36:11 -0500

    Add files via upload
    Used python to generate graph for RQ3-dataset

---
+++ q3DescriptiveAnalysis.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+# Data
+categories = ['Bug-related', 'Error-messages', 'Syntax-challenges', 'Programming-language',
+              'code-optimization', 'conceptual-understanding', 'develop-environment-setup',
+              'version-control', 'Api-usage/Installation', 'security-concerns']
+
+counts = [4, 15, 18, 68, 18, 47, 13, 6, 8, 0]
+total = sum(counts)
+
+# Calculate percentages for the pie chart
+percentages = [(count / total) * 100 for count in counts]
+
+# Plotting the bar chart
+plt.figure(figsize=(10, 8))
+plt.bar(categories, counts, color='lightblue')
+plt.xlabel('Categories')
+plt.ylabel('Percentage')
+plt.title('Recurring Patterns - Counts')
+plt.xticks(rotation=45, ha='right')  # Rotate x-axis labels for better readability
+plt.tight_layout()  # Adjust layout to prevent clipping
+plt.show()
+
+
